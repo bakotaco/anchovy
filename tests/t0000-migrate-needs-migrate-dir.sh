7483572a48e0f3d9d67642be24f657829c9004ec
Bas Kok <bakotaco@gmail.com>
2011-12-19 18:57:08 +0100

    when migrate is run without a migrations directory an error should be raised

--- tests/t0000-migrate-needs-migrate-dir.sh
+++ tests/t0000-migrate-needs-migrate-dir.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
+# When migrate is run without a migrations dir an error should be raised
+
 # helper
 function fail {
     echo "FAIL: $1"
@@ -14,5 +16,11 @@ dir=$(mktemp -d -t migrate-test)
 cd $dir
 
 # run the migrate script and expect it to fail
-out=$($command)
-test $? = 1 || fail "expected an unsuccesful exit status"
+stdout=$($command 2>&1 1>/dev/null)
+exit_status=$?
+
+test $exit_status = 1 || fail "Expected an unsuccesful exit status"
+expected_message=$(echo "ERROR: Directory './migrations' not found.\nDid you initialize migrations for this project?")
+test "$stdout" = "$expected_message" || fail "Did not receive the expected error message"
+
+rmdir $dir
