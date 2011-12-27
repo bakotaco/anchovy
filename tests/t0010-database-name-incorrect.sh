a2f70edb69ec45b56f9185ac5fd46500523e12ec
Bas Kok <bakotaco@gmail.com>
2011-12-27 08:57:40 +0100

    migrate errors when wrong or unprivileged database name is specified

---
+++ tests/t0010-database-name-incorrect.sh
@@ -0,0 +1,39 @@
+#!/bin/sh
+#
+# When a wrong database name is specified, migrate will fail on the first
+# migration. The error for when a non existing and when an existing, but
+# unprivileged database is attempted to be accessed is the same, because we
+# can't determine the difference.
+
+. test-lib.sh
+
+# create an empty directory somewhere which is treated as our 'project'
+# directory for the scope of this test
+dir=$(mktemp -d -t migrate-test)
+cd $dir
+# create a migrations directory with a single dummy migration
+mkdir migrations
+touch migrations/0001-does-nothing.sql
+# all required configuration settings are specified.
+# the following database should not exist or the specified user should not
+# have access to it
+non_existent_db_name="nonexistingdb"
+echo "db_host='localhost'
+db_user='migrate_test'
+db_password='m1gr4t3'
+db_name='$non_existent_db_name'" > migrations/config
+
+# run the migrate script
+stdout=$($migrate_cmd 2>&1)
+exit_status=$?
+
+# expect it to succeed
+expected_message="ERROR: Access denied to database with name '$non_existent_db_name'.
+Is the database name specified in the configuration file correct?
+Does the specified database exist?
+Does the specified user have access privileges to the specified database?"
+assert_equals "$expected_message" "$stdout" "Did not receive the expected error message"
+assert_equals 1 "$exit_status" "Expected an unsuccesful exit status"
+
+# teardown
+rm -rf $dir
