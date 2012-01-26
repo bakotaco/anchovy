f0274c432dd209c8eb966bb8cbd0059b31447a4c
Bas Kok <bakotaco@gmail.com>
2012-01-26 09:43:55 +0100

    initialization fails when database is not empty

---
+++ tests/t0016-init-should-fail-with-nonempty-db.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+#
+# When migrate is run for the first time it should fail when the database
+# already has tables.
+
+. test-lib.sh
+
+create_valid_project
+
+echo "CREATE TABLE foo (bar INT)" | $MYSQL_COMMAND
+
+# run the migrate script
+stdout=$($migrate_cmd 2>&1)
+exit_status=$?
+
+# expect it to fail
+expected_message="* checking for migration tables [MISSING]
+ERROR: database is not empty, will only initialize in an empty database."
+assert_equals "$expected_message" "$stdout" "Did not receive the expected error message"
+assert_equals 1 "$exit_status" "Expected an unsuccesful exit status"
+
+# teardown
+rm -rf $dir
